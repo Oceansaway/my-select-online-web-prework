f3189177e89ae1ae28c148b2e2bac3cd68b1f7af
Stephanie Purple <spurple133@gmail.com>
2019-03-16 02:38:34 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+ i=0
+ new=[]
+ while i < collection.length
+   if yield(collection[i])==true
+     new << collection[i]
+   i += 1
+end
+end
+new
 end
